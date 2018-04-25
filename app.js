c64d7178fe2eeaf8057f30265a9fee03cd49ed4d
Simon Layfield <simonlayfield@gmail.com>
2018-04-25 23:06:10 +1000

    Reverted HTTPS redirects

--- app.js
+++ app.js
@@ -2,16 +2,6 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Evennode
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
-app.use(requireHTTPS);
-
 app.use(express.static('public'));
 
 app.get(['/','index.html'], (req, res) => {
@@ -55,4 +45,5 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}`);
 	console.log('Press Ctrl+C to quit.');
-})
+	console.log('The environment is:', process.env.NODE_ENV);
+});
