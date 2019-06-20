7c6537421babc9c743befc0535f9872ee016aec3
Simon Layfield <simonlayfield@gmail.com>
2019-06-20 14:11:04 +0100

    Added social icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,35 @@
 const gulp = require('gulp'),
-    postcss = require('gulp-postcss'),
-    cssnext = require('postcss-cssnext'),
-    concat = require('gulp-concat'),
-    cssnano = require('cssnano'),
-    gutil = require('gulp-util'),
-	markdownToJSON = require('gulp-markdown-to-json'),
-	marked = require('marked');
+      postcss = require('gulp-postcss'),
+      cssnext = require('postcss-cssnext'),
+      concat = require('gulp-concat'),
+      cssnano = require('cssnano'),
+      gutil = require('gulp-util'),
+    	markdownToJSON = require('gulp-markdown-to-json'),
+    	marked = require('marked');
 
 gulp.task('css', (done) => {
-    gulp.src('src/css/**/*.css')
-        .pipe(concat('main.css'))
-        .pipe(postcss([
-            cssnext(),
-            cssnano()
-        ]))
-        .pipe(gulp.dest('public/css/'));
-        done();
+  gulp.src('src/css/**/*.css')
+    .pipe(concat('main.css'))
+    .pipe(postcss([
+      cssnext(),
+      cssnano()
+    ]))
+    .pipe(gulp.dest('public/css/'));
+    done();
 });
 
 gulp.task('watch', () => {
-    gulp.watch('src/css/**/*.css', gulp.series('css'));
+  gulp.watch('src/css/**/*.css', gulp.series('css'));
 });
 
 gulp.task('blog_json', () => {
 	gulp.src('public/articles/**/*.md')
     .pipe(gutil.buffer())
     .pipe(markdownToJSON(marked, 'blog.json', (data, file) => {
-    delete data.body;
-    data.path = file.path;
-    return data;
-}, {flattenIndex: true}))
+      delete data.body;
+      data.path = file.path;
+      return data;
+    }, {flattenIndex: true}))
     .pipe(gulp.dest('public/articles'))
 });
 
